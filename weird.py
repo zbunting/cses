ce7b6f37455a3f9057872feabb2a8704f8be42bb
Zach Bunting <zvrbunting@gmail.com>
2025-02-11 17:55:23 -0800

    solved

--- weird.py
+++ weird.py
@@ -1,5 +1,9 @@
-# Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
-# $$ 3 \rightarrow 10 \rightarrow 5 \rightarrow 16 \rightarrow 8 \rightarrow 4 \rightarrow 2 \rightarrow 1$$
+# Consider an algorithm that takes as input a positive integer n. If n is even,
+# the algorithm divides it by two, and if n is odd, the algorithm multiplies it
+# by three and adds one. The algorithm repeats this, until n is one. For
+# example, the sequence for n=3 is as follows:
+# $$ 3 \rightarrow 10 \rightarrow 5 \rightarrow 16 \rightarrow 8 \rightarrow 4
+# \rightarrow 2 \rightarrow 1$$
 # Your task is to simulate the execution of the algorithm for a given value of n.
 # Input
 # The only input line contains an integer n.
@@ -16,3 +20,13 @@
 # Output:
 # 3 10 5 16 8 4 2 1
 
+def weird(n):
+    print(n)
+    if n <= 1:
+        return
+    elif n % 2 == 0:
+        weird(n/2)
+    else:
+        weird(n*3+1)
+
+weird(20)
